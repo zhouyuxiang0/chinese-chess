893e2087d810acb2694edc2d16b2ce2d4e2b1d52
zhouyuxiang@epaynfc.com <zhouyuxiang@epaynfc.com>
2022-08-16 18:00:39 +0800

    chore

--- src/chess_game.rs
+++ src/chess_game.rs
@@ -1,36 +1,48 @@
-use std::collections::HashMap;
+use std::sync::Once;
 use yew::prelude::*;
 
+#[derive(Clone, PartialEq)]
 pub struct ChessGame {
-    chess_map: Vec<(i32, i32)>,
-    pieces: Vec<Piece>,
+    pub chess_map: Vec<(i32, i32)>,
+    pub pieces: Vec<Piece>,
 }
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone, PartialEq, Copy)]
 pub enum Group {
     Red,
     Black,
 }
 
+#[derive(Clone, PartialEq, Copy)]
 pub struct Piece {
-    location: (i32, i32),
-    name: &'static str,
-    group: Group,
+    pub location: (i32, i32),
+    pub name: &'static str,
+    pub group: Group,
 }
 
+static mut CHESS_GAME: ChessGame = ChessGame {
+    chess_map: vec![],
+    pieces: vec![],
+};
+static INIT: Once = Once::new();
+
 impl ChessGame {
-    pub fn init_view(&self, x: i32, y: i32) -> Html {
-        ChessGame::init_map();
-        ChessGame::init_piece_info();
-        html! {}
+    pub fn get() -> &'static ChessGame {
+        unsafe {
+            INIT.call_once(|| {
+                CHESS_GAME = ChessGame {
+                    chess_map: ChessGame::init_map(),
+                    pieces: ChessGame::init_piece_info(),
+                };
+            });
+            &CHESS_GAME
+        }
     }
 
     fn init_map() -> Vec<(i32, i32)> {
-        let x = 9;
-        let y = 10;
         let mut chess_map = vec![];
-        for y in 1..=y {
-            for x in 1..=x {
+        for y in 1..=10 {
+            for x in 1..=9 {
                 chess_map.push((x, y))
             }
         }
@@ -123,82 +135,82 @@ impl ChessGame {
             Piece {
                 location: (5, 10),
                 name: "帅",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (1, 10),
                 name: "车",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (9, 10),
                 name: "车",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (2, 10),
                 name: "马",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (8, 10),
                 name: "马",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (2, 8),
                 name: "炮",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (8, 8),
                 name: "炮",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (3, 10),
                 name: "相",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (7, 10),
                 name: "相",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (4, 10),
                 name: "仕",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (6, 10),
                 name: "仕",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (1, 7),
                 name: "兵",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (3, 7),
                 name: "兵",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (5, 7),
                 name: "兵",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (7, 7),
                 name: "兵",
-                group: Group::Black,
+                group: Group::Red,
             },
             Piece {
                 location: (9, 7),
                 name: "兵",
-                group: Group::Black,
+                group: Group::Red,
             },
         ];
         pieces
--- src/chessboard.rs
+++ src/chessboard.rs
@@ -1,4 +1,4 @@
-use crate::chessplaceholder::ChessPlaceHolder;
+use crate::{chess_game::ChessGame, chessplaceholder::ChessPlaceHolder};
 use stylist::yew::{styled_component, use_media_query};
 use yew::prelude::*;
 
@@ -6,14 +6,9 @@ use yew::prelude::*;
 pub fn chessboard() -> Html {
     let is_small = use_media_query("(max-width: 720px)");
     let placeholder_size = if is_small { 50 } else { 70 };
+    let game = ChessGame::get();
     let x = use_state(|| 9);
     let y = use_state(|| 10);
-    let mut chess_map = vec![];
-    for y in 1..=*y {
-        for x in 1..=*x {
-            chess_map.push((x, y));
-        }
-    }
     let (x_size, y_size) = (*x.clone() * placeholder_size, *y.clone() * placeholder_size);
     html! {
         <div class={css!("
@@ -29,7 +24,7 @@ pub fn chessboard() -> Html {
                 grid-template-columns: repeat(9,{}px);
             ", x_size, y_size, placeholder_size)}>
             {
-                chess_map.iter().map(|(x, y)| {
+                game.chess_map.iter().map(|(x, y)| {
                     html!{
                         <ChessPlaceHolder size={placeholder_size} x={*x} y={*y} />
                     }
